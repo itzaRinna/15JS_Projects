3fa021be750b3ba110b3c46eefc8a151de1eafaa
itzaRinna <dutchismahdick@gmail.com>
2024-06-25 08:29:12 +0700

    color flipper done

--- source/01-color-flipper/setup/app.js
+++ source/01-color-flipper/setup/app.js
@@ -1,1 +1,15 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click',function(){
+    const randonNumber = getRandomNumber(); // get random number between 0 - 3 colors[3]
+    console.log(randonNumber);
+    document.body.style.backgroundColor = colors[randonNumber];
+    color.textContent = colors[randonNumber];
+});
+
+getRandomNumber = () => {
+    return Math.floor(Math.random() * colors.length);
+};
+
