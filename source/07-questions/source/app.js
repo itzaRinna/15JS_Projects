099248831d0a8532bcecc096ae5d9467b959b8f9
itzaRinna <dutchismahdick@gmail.com>
2024-07-06 07:56:46 +0700

    question, traversing dom finished

---
+++ source/07-questions/source/app.js
@@ -0,0 +1,11 @@
+//using selectors inside the element
+// traversing the dom
+
+const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach((button)=>{
+    button.addEventListener('click', (e) => {
+        const quest = e.currentTarget.parentElement.parentElement;
+        quest.classList.toggle('show-text');
+    });
+});
