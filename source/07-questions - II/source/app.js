390f12470edaecd878e5c491ac6bed44983bbaa1
itzaRinna <dutchismahdick@gmail.com>
2024-07-06 08:42:23 +0700

    done question, next

--- source/07-questions - II/source/app.js
+++ source/07-questions - II/source/app.js
@@ -1,11 +1,15 @@
 //using selectors inside the element
-// traversing the dom
 
-const btns = document.querySelectorAll('.question-btn');
+const questions = document.querySelectorAll('.question');
 
-btns.forEach((button)=>{
-    button.addEventListener('click', (e) => {
-        const quest = e.currentTarget.parentElement.parentElement;
-        quest.classList.toggle('show-text');
+questions.forEach((question) => {
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click', () => {
+        questions.forEach((item)=>{
+            if(item !== question){
+                item.classList.remove("show-text");
+            }
+        });
+        question.classList.toggle('show-text');
     });
 });
