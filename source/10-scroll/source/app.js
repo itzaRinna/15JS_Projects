e42368703610f8a5ee23bf2bbf2740ee6365d44c
itzaRinna <dutchismahdick@gmail.com>
2024-07-08 14:58:34 +0700

    statically desgined links

--- source/10-scroll/source/app.js
+++ source/10-scroll/source/app.js
@@ -1,13 +1,15 @@
-// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
-// slice extracts a section of a string without modifying original string
-//offsetTop - A Number, representing the top position of the element, in pixels
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
 
-// ********** set date ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
 
-// ********** close links ************
-
-// ********** fixed navbar ************
-
-// ********** smooth scroll ************
-// select links
+navToggle.addEventListener('click', () => {
+    if(!linksContainer.classList.contains("show-links")){
+        linksContainer.classList.add("show-links");
+    }
+    else{
+        linksContainer.classList.remove("show-links");
+    }
+});
