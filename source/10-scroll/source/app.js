d02f167180822e613b6186ab61018093fc126395
itzaRinna <dutchismahdick@gmail.com>
2024-07-09 11:19:33 +0700

    fixed navbar done,next

--- source/10-scroll/source/app.js
+++ source/10-scroll/source/app.js
@@ -6,10 +6,24 @@ const linksContainer = document.querySelector('.links-container');
 const links = document.querySelector('.links');
 
 navToggle.addEventListener('click', () => {
-    if(!linksContainer.classList.contains("show-links")){
-        linksContainer.classList.add("show-links");
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+    if(containerHeight === 0){
+        linksContainer.style.height = `${linksHeight}px`;
     }
     else{
-        linksContainer.classList.remove("show-links");
+        linksContainer.style.height = 0;
+    }
+});
+const navbar = document.getElementById('nav');
+const topLink = document.querySelector('.top-link');
+window.addEventListener("scroll", () => {
+    const scrollHeight = window.pageYOffset;
+    const navHeight = navbar.getBoundingClientRect().height;
+    if(scrollHeight > navHeight){
+        navbar.classList.add('fixed-nav');
+    }
+    else{
+        navbar.classList.remove('fixed-nav');
     }
 });
