08b049e86e0bf8617b77b15711be42a52058566a
itzaRinna <dutchismahdick@gmail.com>
2024-07-12 15:45:53 +0700

    update

--- source/10-scroll/source/app.js
+++ source/10-scroll/source/app.js
@@ -15,8 +15,10 @@ navToggle.addEventListener('click', () => {
         linksContainer.style.height = 0;
     }
 });
+
 const navbar = document.getElementById('nav');
 const topLink = document.querySelector('.top-link');
+
 window.addEventListener("scroll", () => {
     const scrollHeight = window.pageYOffset;
     const navHeight = navbar.getBoundingClientRect().height;
@@ -26,4 +28,36 @@ window.addEventListener("scroll", () => {
     else{
         navbar.classList.remove('fixed-nav');
     }
+
+    if(scrollHeight > 500){
+        topLink.classList.add('show-link');
+    }
+    else{
+        topLink.classList.remove('show-link');
+    }
+});
+
+const scrollLinks = document.querySelectorAll('.scroll-link');
+
+scrollLinks.forEach((link)=>{
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const id = e.currentTarget.getAttribute("href").slice(1);
+        const element = document.getElementById(id);
+        const navHeight = navbar.getBoundingClientRect().height;
+        const containerHeight = linksContainer.getBoundingClientRect().height;
+        const fixedNav = navbar.classList.contains('fixed-nav');
+        let position = element.offsetTop - navHeight;
+        if(!fixedNav){
+            position = position - navHeight;
+        }
+        if(navHeight > 82){
+            position = position + containerHeight;
+        }
+        window.scrollTo({
+            left: 0,
+            top: position,
+        });
+        linksContainer.style.height = 0;
+    });
 });
