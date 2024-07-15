518c192e3531c6f8e1c71693e3d9e8ee4d49be80
itzaRinna <dutchismahdick@gmail.com>
2024-07-15 12:19:34 +0700

    done the basic setup for the functionaility, moving next

--- source/12-countdown-timer/source/app.js
+++ source/12-countdown-timer/source/app.js
@@ -4,7 +4,7 @@ const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
-let futureDate = new Date(2025, 8, 25, 11, 30, 0);
+let futureDate = new Date(2024, 9, 25, 11, 30, 0);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -20,7 +20,26 @@ const futureTime = futureDate.getTime();
 function getRemainingTime(){
   const today = new Date().getTime();
   const t = futureTime - today;
-  console.log(t);
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+
+  let days = Math.floor(t/oneDay);
+  let hours = Math.floor((t % oneDay) / oneHour);
+  let mins = Math.floor((t % oneHour) / oneMinute);
+  let secs = Math.floor((t % oneMinute) / 1000);
+  
+  function format(item){
+    if(item < 10){
+      return item = `0${item}`;
+    }
+    return item;
+  }
+  const values = [days,hours,mins,secs];
+
+  items.forEach((item, index)=>{
+    item.innerHTML = format(values[index]);
+  });
 }
 
 getRemainingTime();
