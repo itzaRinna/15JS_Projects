f4c10543cabe7cdeed1734d2a737241ba302c00a
itzaRinna <dutchismahdick@gmail.com>
2024-07-15 11:57:54 +0700

    changed the structure of the source code, Timelapse Giveaway, next

--- source/12-countdown-timer/source/app.js
+++ source/12-countdown-timer/source/app.js
@@ -1,33 +1,10 @@
-const months = [
-  "January",
-  "February",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "August",
-  "September",
-  "October",
-  "November",
-  "December",
-];
-const weekdays = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+import { months, weekdays } from './index.js'
 
 const giveaway = document.querySelector('.giveaway');
 const deadline = document.querySelector('.deadline');
 const items = document.querySelectorAll('.deadline-format h4');
 
 let futureDate = new Date(2025, 8, 25, 11, 30, 0);
-console.log(futureDate);
 
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -36,4 +13,14 @@ const month = months[futureDate.getMonth()];
 const date = futureDate.getDate();
 const weekday = weekdays[futureDate.getDay()];
 
-giveaway.textContent = `Giveaway ends on ${weekday}, ${date} ${month}, ${year} ${hours}:${mins}am`
+giveaway.textContent = `Giveaway ends on ${weekday}, ${date} ${month}, ${year} ${hours}:${mins}am`;
+
+const futureTime = futureDate.getTime();
+
+function getRemainingTime(){
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  console.log(t);
+}
+
+getRemainingTime();
---
+++ source/12-countdown-timer/source/index.js
@@ -0,0 +1,25 @@
+export const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+];
+export const weekdays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+  
+  
