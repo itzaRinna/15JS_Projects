de4be7a84375ec616869585ab9b0774190f193ed
itzaRinna <dutchismahdick@gmail.com>
2024-07-15 11:19:38 +0700

    future date established, next

--- source/12-countdown-timer/source/app.js
+++ source/12-countdown-timer/source/app.js
@@ -21,3 +21,19 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector('.giveaway');
+const deadline = document.querySelector('.deadline');
+const items = document.querySelectorAll('.deadline-format h4');
+
+let futureDate = new Date(2025, 8, 25, 11, 30, 0);
+console.log(futureDate);
+
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const mins = futureDate.getMinutes();
+const month = months[futureDate.getMonth()];
+const date = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `Giveaway ends on ${weekday}, ${date} ${month}, ${year} ${hours}:${mins}am`
