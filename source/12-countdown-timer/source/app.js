af5c041e4e2c7a8f70e9d857163836e8fc713756
itzaRinna <dutchismahdick@gmail.com>
2024-07-15 12:26:50 +0700

    countdown done, next

--- source/12-countdown-timer/source/app.js
+++ source/12-countdown-timer/source/app.js
@@ -40,6 +40,11 @@ function getRemainingTime(){
   items.forEach((item, index)=>{
     item.innerHTML = format(values[index]);
   });
-}
 
+  if(t < 0){
+    clearInterval(countdown);
+    deadline.innerHTML = `<h4 class="expired">Sorry, this giveaway has expired</h4>`;
+  }
+}
+let countdown = setInterval(getRemainingTime, 1000);
 getRemainingTime();
