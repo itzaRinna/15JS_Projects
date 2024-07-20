ee673683f211b04df686f2244b98e49e1458b9d8
itzaRinna <dutchismahdick@gmail.com>
2024-07-20 07:28:25 +0700

    basic logic added

--- source/14-grocery-bud/source/app.js
+++ source/14-grocery-bud/source/app.js
@@ -1,11 +1,26 @@
-// ****** SELECT ITEMS **********
+const alert = document.querySelector('.alert');
+const form = document.querySelector('.grocery-form');
+const grocery = document.getElementById('grocery');
+const submitBtn = document.querySelector('.submit-btn');
+const container = document.querySelector('.grocery-container');
+const list = document.querySelector('.grocery-list');
+const clearBtn = document.querySelector('.clear-btn');
 
-// edit option
+let editElement;
+let editFlag = false;
+let editID = "";
 
-// ****** EVENT LISTENERS **********
+form.addEventListener('submit', addItem);
 
-// ****** FUNCTIONS **********
-
-// ****** LOCAL STORAGE **********
-
-// ****** SETUP ITEMS **********
+function addItem(e){
+    e.preventDefault();
+    const value = grocery.value;
+    const id = new Date().getTime().toString();
+    if(value && !editFlag){
+        console.log('adding');
+    }else if(value && editFlag){
+        console.log('editing');
+    }else{
+        console.log('empty value');
+    }
+}
